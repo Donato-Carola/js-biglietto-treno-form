a8bfd165dd019e0194a1e577c35eddb0f04cd4d3
Donato-Carola <don94@live.it>
2023-11-13 16:50:35 +0100

    inserimento della funzine con i suoi ripsettivi calcoli all'interno

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 /*Descrizione:
 Scrivere un programma che chieda all’utente:
 
@@ -18,29 +17,53 @@ Solo una volta che il milestone 1 sarà completo e funzionante allora realizzere
 Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
 Questo richiederà un minimo di ricerca.*/
 
-
 // ? inserire dati del passeggero
 
-
-                                                                 // *! PRIMA PROVA SU COME LAVORARE SU UNA FUNZIONE
-                                                                 
-const buttonElement = document.querySelector('button');
-
-buttonElement.addEventListener('click',
-function()   {
-   
-    const kmUser = parseFloat(document.getElementById('km').value);
-   
-
-    console.log(kmUser)
-
-    const ageUser = parseInt(document.getElementById('age').value);
-    console.log(ageUser);
-
-    let somma = kmUser + ageUser;
-    console.log(somma);
+// *! PRIMA PROVA SU COME LAVORARE SU UNA FUNZIONE
 
 
-   
 
+
+const buttonElement = document.querySelector("button");
+
+
+
+
+buttonElement.addEventListener("click", 
+function () {
+
+  //* valore dei kilometri inseriti nel tag html INPUT con id km(kilomteri)
+  
+  const kmUser = parseFloat(document.getElementById("km").value);
+  console.log(kmUser);
+
+  //* valore dell'età inseriti nel tag html INPUT con id age(età)
+
+  const ageUser = parseInt(document.getElementById("age").value);
+  console.log(ageUser);
+
+  //* calcolo del prezzo in base ai km (0.1976 euro al km)
+
+  const calcPriceKm = kmUser * 0.1976;
+  console.log(`'calcolo prezzo moltiplicato per 0.1976 euro: ' ${calcPriceKm}`);
+
+  //* una volta avuto il prezzo totale possiamo fare le varie operazioni con IF
+
+                                 // ! prima condizione calcolare lo sconto del 17,65% se sono minorenni
+
+  if (ageUser < 18) {
+    const discount = (calcPriceKm * 17.65) / 100;
+    const priceTotale = (calcPriceKm - discount).toFixed(2);
+    console.log(`'il prezzo con lo sconto del 17.65% è: ' ${priceTotale}`);
+
+                                // ! seconda condizione calcolare lo sconto del 53.27% se sono minorenni
+  } else if (ageUser >= 65) {
+    const discount = (calcPriceKm * 53.27) / 100;
+    const priceTotale = (calcPriceKm - discount).toFixed(2);
+    console.log(`'il prezzo con lo sconto del 53.27% è: ' ${priceTotale}`);
+
+                               //!terza condizione se l'età non rientra nello sconto
+  } else {
+    console.log(`'il prezzo senza sconto è:' ${calcPriceKm}`);
+  }
 });
